01a2301793a9bdf94dfd12673681cb70e2882dae
WholeCode <ciropandxans@gmail.com>
2023-03-08 21:56:53 +0300

    add team section

--- js/main.js
+++ js/main.js
@@ -5,5 +5,12 @@ $(function () {
     $('.directions__filter-btn').on('click', function () {
         $('.directions__filter-btn').removeClass('directions__filter-btn--active')
         $(this).addClass('directions__filter-btn--active')
-    })
-})
+    });
+    
+    $('.team__slider').slick({
+        arrows: false,
+        slidesToShow: 4,
+        infinite: true,
+        draggable:false,
+});
+});
